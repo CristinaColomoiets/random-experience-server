415b3ff18566096a6973b8f81c103b3f813eb889
CristinaColomoiets <colomoetscristina@gmail.com>
2024-05-30 09:52:55 +0200

    Changed destructuring of object payload for render userimage

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -75,9 +75,9 @@ router.post('/login', (req, res, next) => {
                 return
             }
 
-            const { _id, email, username } = foundUser
+            const { _id, email, username, image } = foundUser
 
-            const payload = { _id, email, username }
+            const payload = { _id, email, username, image }
 
             const authToken = jwt.sign(
                 payload,
